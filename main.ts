084cee9caf51478565462739c0320e05cb8fe5ab
paul boon <paul.boon@perplex.nl>
2020-08-25 16:43:06 +0200

    p

--- main.ts
+++ main.ts
@@ -32,6 +32,7 @@ for(let hotbar of hotbars){
 class Trigger{
     constructor(
         public char:string,
+        public actionid:number,
         public shift:boolean,
         public ctrl:boolean,
         public alt:boolean,
@@ -40,10 +41,48 @@ class Trigger{
     }
 }
 
+class Action{
+    constructor(
+        public id:number,
+        public description:string,
+        public cb:() => void,
+    ){
 
-var triggers = []//q,w,e,r,t,y   //rebindable(via menu~ or clicking on the tooltip)
-var actions = []//action bar 1,2,3,4,5,6,7
+    }
+}
+
+
+var triggers = [
+    new Trigger('q',0,false,false,false),
+    new Trigger('w',1,false,false,false),
+    new Trigger('e',2,false,false,false),
+    new Trigger('r',3,false,false,false),
+]//q,w,e,r,t,y   //rebindable(via menu~ or clicking on the tooltip)
+
+var actions = [
+    new Action(0, 'trigger actionbar', () => triggerActionBar(0)),
+    new Action(1, 'trigger actionbar', () => triggerActionBar(1)),
+    new Action(2, 'trigger actionbar', () => triggerActionBar(2)),
+    new Action(3, 'trigger actionbar', () => triggerActionBar(3)),
+]//action bar 1,2,3,4,5,6,7
+
+function triggerActionBar(index:number){
+    console.log(index);
+    //find hotbar
+    //get spell
+    //trigger spell
+}
 
 document.addEventListener('keydown', e => {
     
+    var hittriggers = triggers.filter(t => {
+        return t.char == e.key
+        && t.ctrl == e.ctrlKey
+        && t.shift == e.shiftKey
+        && t.alt == e.altKey
+    })
+    for(var hittrigger of hittriggers){
+        actions.filter(a => a.id == hittrigger.actionid).forEach(a => a.cb())
+    }
+
 })
