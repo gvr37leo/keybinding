a582748c0464235929d4d2f428a1b03252592e25
Paul Boon <gvr37.p.l.boon@gmail.com>
2020-08-29 21:07:59 +0200

    p

--- ability.ts
+++ ability.ts
@@ -15,6 +15,12 @@ class Ability2{
         this.element = string2html(`<div class="ability" draggable="true">${this.name}</div>`)
         this.element.addEventListener('dragstart', ev => {
             ev.dataTransfer.setData("abilityid", this.id as any);
+            // var ghost = this.element.cloneNode(true) as HTMLElement
+            // document.body.appendChild(ghost)
+            // ghost.style.position = 'absolute'
+            // ghost.style.top = '-5000px'
+            // ghost.style.top = '-5000px'
+            // ev.dataTransfer.setDragImage(this.element as any,0,0)
         })
 
         this.element.addEventListener('click',() => {
